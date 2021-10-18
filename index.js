da9ae3d52f3f838d8b28acd864a54c4b0c67ff02
Adrian Hansa <adrian.hansa@yahoo.com>
2021-10-18 17:43:36 +0100

    Initial setup of the express server; db coonection setup.

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log(`DB connected!`);
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+  })
+  .catch((error) => console.log(error.message));
